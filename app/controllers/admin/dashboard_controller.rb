eb6c6cdacfe99854975891dafa61b2e3c67bb068
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-14 16:57:08 -0500

    Feature: Admin Dashboard - Count Challenge

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+
   def show
+    @categories = Category.all.order(created_at: :desc)
+    @products = Product.all.order(created_at: :desc)
   end
+
 end
