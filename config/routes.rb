c6d6c10d432dfabcb5d85530b880d93d2949eaa2
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-15 13:06:17 -0500

    Feature: User Authentication Jungle Challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   resources :about, only: [:index, :show]
 
   resources :products, only: [:index, :show]
