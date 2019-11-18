07587bcfb28855c3c5cfe09562fea38312a1d226
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-18 14:44:46 -0500

    Added shoulda-matchers in the Gemfile and rails_helper.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
+# Should hav five examples defined given that you have four validations
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
+    # @category = Category.new
+    # @product = Product.new
+    it { is_expected.to validate_presence_of(:name)}
+    it { is_expected.to validate_presence_of(:price)}
+    it { is_expected.to validate_presence_of(:quantity)}
+    it { is_expected.to validate_presence_of(:category)}
   end
 end
+
+
