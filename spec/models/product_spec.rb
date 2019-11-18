1c46313f9518ba8e0c22c1135f85ddcf057a588e
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-18 18:11:00 -0500

    Changed syntax of pending examples

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,12 +4,17 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
-    # @category = Category.new
-    # @product = Product.new
-    it { is_expected.to validate_presence_of(:name)}
-    it { is_expected.to validate_presence_of(:price)}
-    it { is_expected.to validate_presence_of(:quantity)}
-    it { is_expected.to validate_presence_of(:category)}
+    category = Category.new
+    @product = Product.new
+    it "is valid with valid attributes"
+    it "is not valid without a name"
+    it "is not valid without a price"
+    it "is not valid without a quantity"
+    it "is not valid without a category"
+    # it { is_expected.to validate_presence_of(:name)}
+    # it { is_expected.to validate_presence_of(:price)}
+    # it { is_expected.to validate_presence_of(:quantity)}
+    # it { is_expected.to validate_presence_of(:category)}
   end
 end
 
