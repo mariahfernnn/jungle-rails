e26afa37a7513106b906604c588832720e60a9d4
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-18 19:21:46 -0500

    Model spec: Product - Jungle Assignment

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,17 +4,52 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
-    category = Category.new
-    @product = Product.new
-    it "is valid with valid attributes"
-    it "is not valid without a name"
-    it "is not valid without a price"
-    it "is not valid without a quantity"
-    it "is not valid without a category"
-    # it { is_expected.to validate_presence_of(:name)}
-    # it { is_expected.to validate_presence_of(:price)}
-    # it { is_expected.to validate_presence_of(:quantity)}
-    # it { is_expected.to validate_presence_of(:category)}
+    # subject { described_class.new }
+
+    it "is valid with valid attributes" do
+      @category = Category.new
+      @category.name = 'Shoes'
+      subject { described_class.new }
+      subject.name = 'Hipster combat boots'
+      subject.price = 240.00
+      subject.quantity = 2
+      subject.category = @category
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a name" do
+      @category = Category.new
+      subject { described_class.new }
+      expect(subject).to_not be_valid
+      puts subject.errors.full_messages
+    end
+
+    it "is not valid without a price" do
+      @category = Category.new
+      subject { described_class.new }
+      subject.name = 'Hipster combat boots'
+      expect(subject).to_not be_valid
+      puts subject.errors.full_messages
+    end
+
+    it "is not valid without a quantity" do
+      @category = Category.new
+      subject { described_class.new }
+      subject.name = 'Hipster combat boots'
+      subject.price = 240.00
+      expect(subject).to_not be_valid
+      puts subject.errors.full_messages
+    end
+
+    it "is not valid without a category" do
+      @category = Category.new
+      subject { described_class.new }
+      subject.name = 'Hipster combat boots'
+      subject.price = 240.00
+      subject.quantity = 2
+      expect(subject).to_not be_valid
+      puts subject.errors.full_messages
+    end
   end
 end
 
