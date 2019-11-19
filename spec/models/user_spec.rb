764a62b15aceddf4c6432241b9479dd0cb5406fa
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-18 19:48:54 -0500

    Step 2: Define validation specs - first it block complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe UserSpec, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it "is valid with valid attributes" do
+      expect(User.new).to be_valid
+    end
+    
+    it "is valid when password and password_confirmation match"
+    it "is not valid if password and password_confirmation do not match"
+    it "is valid if the email matches the email in the database"
+  end
 end
