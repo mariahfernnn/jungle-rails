67fa163ed456ce2d8a8ea7fbaf212d32b9a752bd
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-18 20:53:45 -0500

    Added validations in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  # EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  validates :name, presence: true
+  validates :email, presence: true, confirmation: { case_sensitive: false }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
   
 end
