27afa7f7305cc9f0038c05d5e29331193fbdd2dc
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-18 21:07:52 -0500

    All specs for step 2 have now been validated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  # EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-  validates :name, presence: true
-  validates :email, presence: true, confirmation: { case_sensitive: false }
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+  # validates :name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  # validates :password, confirmation: true
+  # validates :password_confirmation, presence: true
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,16 +4,24 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
 
     it "is valid with valid attributes" do
-      user = User.new(name: 'Maria', email: 'mf@test.com', password: 'test', password_confirmation: 'test')
+      user = User.create(name: 'Maria', email: 'mf@test.com', password: 'test', password_confirmation: 'test')
       expect(user).to be_valid
     end
 
     it "is valid when password and password_confirmation match" do
-      user = User.new(name: 'Maria', email: 'mf@test.com', password: 'test', password_confirmation: 'test')
+      user = User.create(name: 'Maria', email: 'mf@test.com', password: 'test', password_confirmation: 'test')
       expect(user).to be_valid
     end
 
-    it "is not valid if password and password_confirmation do not match"
-    it "is valid if the email matches the email in the database" 
+    it "is not valid if password and password_confirmation do not match" do
+      user = User.create(name: 'Maria', email: 'mf@test.com', password: 'test', password_confirmation: 'TEST')
+      expect(user).to_not be_valid
+    end
+
+    it "is not valid if the email matches an email that is already in the database" do
+      user = User.create(name: 'Maria', email: 'TEST@TEST.com', password: 'test', password_confirmation: 'test')
+      user2 = User.create(name: 'Maria', email: 'test@test.COM', password: 'test', password_confirmation: 'test')
+      expect(user2).to_not be_valid
+    end
   end
 end
