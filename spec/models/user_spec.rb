cd4821bc7f6b6883bcf6528fa14d4143a6579331
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-18 20:12:18 -0500

    Validated the following cases: valid with valid attributes, valid when password and password_confirmation match, is not valid if password and password_confirmation do not match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
+
     it "is valid with valid attributes" do
-      expect(User.new).to be_valid
+      user = User.new(name: 'Maria', email: 'mf@test.com', password: 'test', password_confirmation: 'test')
+      expect(user).to be_valid
     end
-    
-    it "is valid when password and password_confirmation match"
-    it "is not valid if password and password_confirmation do not match"
-    it "is valid if the email matches the email in the database"
+
+    it "is valid when password and password_confirmation match" do
+      user = User.new(password: 'test', password_confirmation: 'test')
+      expect(user).to be_valid
+    end
+
+    it "is not valid if password and password_confirmation do not match" do
+      user = User.new(password: 'test', password_confirmation: 'TEST')
+      expect(user).to_not be_valid
+    end
+
+    it "is valid if the email matches the email in the database" do
+      user = User.new(email: nil)
+      expect(user).to_not be_valid
+    end
   end
 end
