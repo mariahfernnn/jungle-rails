0acc991d904bbdf6589570d01f635b28ef1b3d65
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-18 20:54:34 -0500

    First two it blocks passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,18 +9,11 @@ RSpec.describe User, type: :model do
     end
 
     it "is valid when password and password_confirmation match" do
-      user = User.new(password: 'test', password_confirmation: 'test')
+      user = User.new(name: 'Maria', email: 'mf@test.com', password: 'test', password_confirmation: 'test')
       expect(user).to be_valid
     end
 
-    it "is not valid if password and password_confirmation do not match" do
-      user = User.new(password: 'test', password_confirmation: 'TEST')
-      expect(user).to_not be_valid
-    end
-
-    it "is valid if the email matches the email in the database" do
-      user = User.new(email: nil)
-      expect(user).to_not be_valid
-    end
+    it "is not valid if password and password_confirmation do not match"
+    it "is valid if the email matches the email in the database" 
   end
 end
