7f2d5c66c39fff0246d41d119ab8f5083042f9eb
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-20 09:29:36 -0500

    Added the setup code from the home_page feature and added js: true to use the javascript-enabled browser

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates from the home page to the product detail page by clicking on a product", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+end
