808ad048f4675fde38f74bb34f2dc8d8f02f5b55
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-20 21:24:58 -0500

    Feature Spec: Add to Cart - Jungle Assignment

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor can click the Add to Cart button", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
-
+    
     10.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
@@ -16,15 +16,17 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "The cart increases by one" do
     # ACT
     visit root_path
+    page.first('.add-to-cart').click
 
     # DEBUG
+    # puts page.html
     # save_and_open_screenshot
 
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    within('#navbar'){expect(page).to have_content('Home About Categories Admin Login Signup My Cart (1)')}
   end
-
+2
 end
